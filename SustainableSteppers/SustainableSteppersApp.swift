43e103ee2e1d401f5dd1269cab0c2ca7626431fe
Krishi <krishi3dalal@gmail.com>
2023-07-17 14:49:12 -0400

    Initial Commit

---
+++ SustainableSteppers/SustainableSteppersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainableSteppersApp.swift
+//  SustainableSteppers
+//
+//  Created by Krishi Dalal on 2023-07-17.
+//
+
+import SwiftUI
+
+@main
+struct SustainableSteppersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
